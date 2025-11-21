03f5d7ae15a975e2cbf3fe1c57daade2459da91a
ashreekar <ashreekar767@gmail.com>
2025-11-21 18:58:47 +0530

    add: channel model

---
+++ src/model/Channel.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const channelSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            index: true,
+            trim: true,
+            minlength: 3,
+            maxlength: 50
+        },
+        handle: {
+            type: String,
+            required: true,
+            unique: true,
+            index: true,
+            trim: true
+        },
+        avatar: {
+            type: String
+        },
+        banner: {
+            type: String
+        },
+        videos: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        subscribers: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ],
+        playlist: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Playlist"
+            }
+        ],
+        posts: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Post"
+            }
+        ],
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Channel = mongoose.model("Channel", channelSchema);
