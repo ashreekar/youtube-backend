a52cb02e8c4586613fe760a1eeed4b32641df6d6
ashreekar <ashreekar767@gmail.com>
2025-11-22 11:37:44 +0530

    add:Post model

---
+++ src/model/Post.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+    {
+        content: {
+            type: String,
+            required: true
+        },
+        images: {
+            type: String
+        },
+        postedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Channel"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Post = mongoose.model("Post", postSchema);
