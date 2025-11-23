82f5da3cb5a7c2a4254b2e1117a713b17206b5b7
ashreekar <ashreekar767@gmail.com>
2025-11-23 22:58:10 +0530

    add: comment controller and route

---
+++ src/controller/comment.controller.js
@@ -0,0 +1,369 @@
+import { APIerror } from "../util/APIerror.js";
+import { APIresponse } from "../util/APIresponse.js";
+import { asyncHandler } from "../util/asyncHandler.js";
+
+import { Comment } from "../model/Comment.model.js";
+import { Video } from "../model/Video.model.js";
+import { Post } from "../model/Post.model.js";
+
+const commentOnVideo = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const { content } = req.body;
+
+    if (!content) {
+        throw new APIerror(404, "Comment body is empty");
+    }
+
+    const video = await Video.findById(id);
+
+    if (!video) {
+        throw new APIerror(400, "Video not found with id");
+    }
+
+    const commentExists = await Comment.findOne(
+        {
+            $and: [{ video: id }, { commenter: req.user._id }]
+        }
+    )
+
+    if (commentExists) {
+        throw new APIerror(400, "Comment already exists");
+    }
+
+    await Comment.create(
+        {
+            content: content,
+            commenter: req.user._id,
+            video: id
+        }
+    )
+
+    res.status(201).json(new APIresponse(201, {}, "comment added"));
+})
+
+const commentOnCommnent = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const { content } = req.body;
+
+    if (!content) {
+        throw new APIerror(404, "Comment body is empty");
+    }
+
+    const comment = await Comment.findById(id);
+
+    if (!comment) {
+        throw new APIerror(400, "Comment not found with id");
+    }
+
+    const commentExists = await Comment.findOne(
+        {
+            $and: [{ comment: id }, { commenter: req.user._id }]
+        }
+    )
+
+    if (commentExists) {
+        throw new APIerror(400, "Comment already exists");
+    }
+
+    await Comment.create(
+        {
+            content: content,
+            commenter: req.user._id,
+            comment: id
+        }
+    )
+
+    res.status(201).json(new APIresponse(201, {}, "comment added"));
+})
+
+const commentOnPost = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const { content } = req.body;
+
+    if (!content) {
+        throw new APIerror(404, "Comment body is empty");
+    }
+
+    const post = await Post.findById(id);
+
+    if (!post) {
+        throw new APIerror(400, "Post not found with id");
+    }
+
+    const commentExists = await Comment.findOne(
+        {
+            $and: [{ post: id }, { commenter: req.user._id }]
+        }
+    )
+
+    if (commentExists) {
+        throw new APIerror(400, "Comment already exists");
+    }
+
+    await Comment.create(
+        {
+            content: content,
+            commenter: req.user._id,
+            post: id
+        }
+    )
+
+    res.status(201).json(new APIresponse(201, {}, "comment added"));
+})
+
+const getCommentOfVideo = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const comments = await Comment.aggregate(
+        [
+            {
+                $match: { video: id }
+            },
+            {
+                $lookup: {
+                    from: "comments",
+                    localField: "video",
+                    foreignField: "_id",
+                    as: "comments"
+                }
+            },
+              {
+                $lookup: {
+                    from: "reactions",
+                    localField: "_id",
+                    foreignField: "comment",
+                    as: "reactions"
+                }
+            },
+            {
+                $addFields: {
+                    totalComments: {
+                        $size: "$comments"
+                    },
+                    likes:{
+                        $size:{
+                            $filter:{
+                                from:"$reactions",
+                                as:"reaction",
+                                cond: { $eq: ["$$reaction.type", "like"] }
+                            }
+                        }
+                    },
+                     dislikes:{
+                        $size:{
+                            $filter:{
+                                from:"$reactions",
+                                as:"reaction",
+                                cond: { $eq: ["$$reaction.type", "dislike"] }
+                            }
+                        }
+                    },
+                }
+            },
+            {
+                $addFields: {
+                    totalComments: {
+                        $size: "$comments"
+                    }
+                }
+            },
+            {
+                $project: {
+                    totalComments: 1,
+                    comments: 1,
+                     likes: 1,
+                    dislikes: 1,
+                }
+            }
+        ]
+    )
+
+    res.status(200).json(new APIresponse(200, comments, "Comments sent sucessfully"));
+})
+
+const getCommentOfPost = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const comments = await Comment.aggregate(
+        [
+            {
+                $match: { post: id }
+            },
+            {
+                $lookup: {
+                    from: "comments",
+                    localField: "post",
+                    foreignField: "_id",
+                    as: "comments"
+                }
+            },
+              {
+                $lookup: {
+                    from: "reactions",
+                    localField: "_id",
+                    foreignField: "comment",
+                    as: "reactions"
+                }
+            },
+            {
+                $addFields: {
+                    totalComments: {
+                        $size: "$comments"
+                    },
+                    likes:{
+                        $size:{
+                            $filter:{
+                                from:"$reactions",
+                                as:"reaction",
+                                cond: { $eq: ["$$reaction.type", "like"] }
+                            }
+                        }
+                    },
+                     dislikes:{
+                        $size:{
+                            $filter:{
+                                from:"$reactions",
+                                as:"reaction",
+                                cond: { $eq: ["$$reaction.type", "dislike"] }
+                            }
+                        }
+                    },
+                }
+            },
+            {
+                $addFields: {
+                    totalComments: {
+                        $size: "$comments"
+                    }
+                }
+            },
+            {
+                $project: {
+                    totalComments: 1,
+                    comments: 1,
+                     likes: 1,
+                    dislikes: 1,
+                }
+            }
+        ]
+    )
+
+    res.status(200).json(new APIresponse(200, comments, "Comments sent sucessfully"));
+})
+
+const getCommentOfComment = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const comments = await Comment.aggregate(
+        [
+            {
+                $match: { post: id }
+            },
+            {
+                $lookup: {
+                    from: "comments",
+                    localField: "comment",
+                    foreignField: "_id",
+                    as: "comments"
+                }
+            },
+            {
+                $lookup: {
+                    from: "reactions",
+                    localField: "_id",
+                    foreignField: "comment",
+                    as: "reactions"
+                }
+            },
+            {
+                $addFields: {
+                    totalComments: {
+                        $size: "$comments"
+                    },
+                    likes:{
+                        $size:{
+                            $filter:{
+                                from:"$reactions",
+                                as:"reaction",
+                                cond: { $eq: ["$$reaction.type", "like"] }
+                            }
+                        }
+                    },
+                     dislikes:{
+                        $size:{
+                            $filter:{
+                                from:"$reactions",
+                                as:"reaction",
+                                cond: { $eq: ["$$reaction.type", "dislike"] }
+                            }
+                        }
+                    },
+                }
+            },
+            {
+                $project: {
+                    totalComments: 1,
+                    comments: 1,
+                     likes: 1,
+                    dislikes: 1,
+                    createdAt: 1
+                }
+            }
+        ]
+    )
+
+    res.status(200).json(new APIresponse(200, comments, "Comments sent sucessfully"));
+})
+
+const deleteComment = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const comment = await Comment.findOneAndDelete(
+        {
+            $and: [{ _id: id }, { commenter: req.user._id}]
+        }
+    );
+
+    if (comment) {
+        throw new APIerror(404, "Comment not found or can't delete this comment");
+    }
+
+    await Comment.deleteMany(
+        {
+            comment: id
+        }
+    )
+
+    res.status(200).json(new APIresponse(200, "Comment deleted"));
+})
+
+const updateComment = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const { content } = req.body;
+
+    const comment = await Comment.findOneAndUpdate(
+        {
+            $and: [{ _id: id }, { commenter: req.user._id}]
+        }, 
+        { 
+            content 
+        }
+    );
+
+    if (comment) {
+        throw new APIerror(404, "Comment not found");
+    }
+
+    res.status(200).json(new APIresponse(200, "Comment deleted"));
+})
+
+export {
+    commentOnCommnent,
+    commentOnPost,
+    commentOnVideo,
+    getCommentOfVideo,
+    getCommentOfPost,
+    getCommentOfComment,
+    deleteComment,
+    updateComment
+};
---
+++ src/route/comment.route.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import { verifyJwt } from "../middleware/verifyJwt.js"
+import {
+    commentOnCommnent,
+    commentOnPost,
+    commentOnVideo,
+    deleteComment,
+    getCommentOfComment,
+    getCommentOfPost,
+    getCommentOfVideo,
+    updateComment,
+} from "../controller/comment.controller.js";
+
+const router = Router();
+
+router
+    .route('/:id')
+    .put(verifyJwt, updateComment)
+    .delete(verifyJwt, deleteComment)
+
+router
+    .route('/video/:id')
+    .get(getCommentOfVideo)
+    .post(verifyJwt, commentOnVideo)
+
+router
+    .route('/comment/:id')
+    .get(getCommentOfComment)
+    .post(verifyJwt, commentOnCommnent)
+
+router
+    .route('/post/:id')
+    .get(getCommentOfPost)
+    .post(verifyJwt, commentOnPost)
+
+export { router };
