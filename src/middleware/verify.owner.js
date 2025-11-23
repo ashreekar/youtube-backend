54bfadcda343fd5e15e4812fe77c61a244a5fdf9
ashreekar <ashreekar767@gmail.com>
2025-11-23 18:05:42 +0530

    add:verify owner middleware

---
+++ src/middleware/verify.owner.js
@@ -0,0 +1,40 @@
+import { Playlist } from "../model/Playlist.model.js";
+import { Post } from "../model/Post.model.js";
+import { Video } from "../model/Video.model.js";
+import { APIerror } from "../util/APIerror.js";
+import { asyncHandler } from "../util/asyncHandler.js";
+
+export const verifyOwner = asyncHandler(async (req, res, next) => {
+
+    const video = await Video.findOne(
+        {
+            $and: [{ _id: req.params.id }, { owner: req.channel._id }]
+        }
+    )
+
+    if (video) {
+        return next();
+    }
+
+    const post = await Post.findOne(
+        {
+            $and: [{ _id: req.params.id }, { postedBy: req.channel._id }]
+        }
+    )
+
+    if (post) {
+        return next();
+    }
+
+    const playlist = await Playlist.findOne(
+        {
+            $and: [{ _id: req.params.id }, { createdBy: req.channel._id }]
+        }
+    )
+
+    if (playlist) {
+        return next();
+    }
+
+    throw new APIerror(400, "Invalid acceas to modify resource");
+});
