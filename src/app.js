7849882a3a40054936b3aa0f8d191fcbdb37677e
ashreekar <ashreekar767@gmail.com>
2025-12-25 00:52:46 +0530

    fix:change path quote

--- src/app.js
+++ src/app.js
@@ -3,17 +3,17 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 // Routes
-import userRouter from "./route/user.route.js"
-import channelRouter from "./route/channel.route.js"
-import vidoeRouter from "./route/video.route.js"
-import postRouter from "./route/post.route.js"
-import commentRouter from "./route/comment.route.js"
-import reactionRouter from "./route/reaction.route.js"
-import playlistRouter from "./route/playlist.route.js"
-import searchRouter from "./route/search.route.js"
+import userRouter from './route/user.route.js'
+import channelRouter from './route/channel.route.js'
+import videoRouter from './route/video.route.js'
+import postRouter from './route/post.route.js'
+import commentRouter from './route/comment.route.js'
+import reactionRouter from './route/reaction.route.js'
+import playlistRouter from './route/playlist.route.js'
+import searchRouter from './route/search.route.js'
 
 //  global error handler
-import { errorHandler } from "./middleware/error.middleware.js"
+import { errorHandler } from './middleware/error.middleware.js'
 
 const app = express();
 
@@ -37,7 +37,7 @@ app.use(cookieParser());
 
 // registering all routes
 app.use('/api/v1/user', userRouter);
-app.use('/api/v1/video', vidoeRouter);
+app.use('/api/v1/video', videoRouter);
 app.use('/api/v1/channel', channelRouter);
 app.use('/api/v1/post', postRouter);
 app.use('/api/v1/comment', commentRouter);
